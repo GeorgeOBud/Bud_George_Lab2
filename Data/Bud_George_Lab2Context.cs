7dff3908d45de18d9cc73d06e512269b749a986b
GeorgeOBud <george.bud@stud.ubbcluj.ro>
2023-11-06 14:19:45 +0200

    Add project files.

---
+++ Data/Bud_George_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bud_George_Lab2.Models;
+
+namespace Bud_George_Lab2.Data
+{
+    public class Bud_George_Lab2Context : DbContext
+    {
+        public Bud_George_Lab2Context (DbContextOptions<Bud_George_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bud_George_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Bud_George_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Bud_George_Lab2.Data;
+using Bud_George_Lab2.Models;
+
+namespace Bud_George_Lab2.Pages.Publishers
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Bud_George_Lab2.Data.Bud_George_Lab2Context _context;
+
+        public DetailsModel(Bud_George_Lab2.Data.Bud_George_Lab2Context context)
+        {
+            _context = context;
+        }
+
+      public Publisher Publisher { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Publisher == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publisher.FirstOrDefaultAsync(m => m.ID == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Publisher = publisher;
+            }
+            return Page();
+        }
+    }
+}
