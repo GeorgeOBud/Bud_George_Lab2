403258c67c01aa4de123999b04166db75066c508
GeorgeOBud <george.bud@stud.ubbcluj.ro>
2023-11-08 21:23:19 +0200

    Finishing touches 2

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,9 +1,11 @@
-﻿namespace Bud_George_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Bud_George_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
-        public string? FirstName { get; set; }
+        public string? FirstName {  get; set; }
         public string? LastName { get; set; }
 
     }
